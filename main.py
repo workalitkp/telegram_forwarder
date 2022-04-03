d4d87987d6f55377c99bfd4f318908566b3c4fe7
alitkp <workalitkp@gmail.com>
2022-04-03 15:36:35 +0430

    completed clean code for caption based posts

--- main.py
+++ main.py
@@ -19,11 +19,25 @@ def start(update: Update, context: CallbackContext):
 
 
 def remove_tag(caption):
-    return re.sub(r'(https?:\/\/)?(www[.])?(telegram|t)\.me\/([a-zA-Z0-9_-]*)\/?','',caption)
-
+    try:
+        found = re.search('(https?:\/\/)?(www[.])?(telegram|t)\.me\/([a-zA-Z0-9_-]*)\/?', caption).groups()[-1]
+    except AttributeError:
+        found = None
+    if (not found):
+        try:
+            found = re.search('\B@\S+', caption).group(0)[1:]
+        except AttributeError:
+            found = None
+    caption = re.sub(r'(https?:\/\/)?(www[.])?(telegram|t)\.me\/([a-zA-Z0-9_-]*)\/?', '', caption)
+    caption = re.sub(r'\B@\S+', '', caption)
+    return caption,found
+    # return
 def clean_caption(caption):
-    cleaned = remove_tag(caption)
-    cleaned += "\n\njoin {} \nsource: {}".format(CHANNEL,"source")
+    cleaned , source = remove_tag(caption)
+    if(source):
+        cleaned += "\n\njoin {} \nsource: {}".format(CHANNEL,source)
+    else:
+        cleaned += "\n\njoin {}".format(CHANNEL)
     return cleaned
 def forward_post(update: Update, context: CallbackContext):
     context.bot.copy_message(chat_id=CHANNEL, from_chat_id=update.effective_chat.id, message_id=update.message.message_id,caption=clean_caption(update.message.caption))
